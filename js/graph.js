9c0bc35dde0195edf43f7560d568f5dc081ff725
David M <davidmosk@gmail.com>
2019-09-30 11:31:37 +0800

    graphing script with options

---
+++ js/graph.js
@@ -0,0 +1,64 @@
+
+
+function addData(chart, data) {
+    // chart.data.labels.push(label);
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data = data;
+
+    });
+    chart.update();
+}
+
+// yNumbers = [4,3,5,8,7,7,4,3,7.8,3]
+
+var ctx = document.getElementById('myChart');
+var myChart = new Chart(ctx, {
+    type: 'horizontalBar',
+    data: {
+        labels: [2,3,4,5,6,7,8,9,10,11,12],
+        datasets: [{
+            label: 'Dice Roll Distribution',
+            data: yNumbers,
+            backgroundColor: [
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 255, 255, 1)',
+                'rgba(255, 255, 255, 1)',
+                'rgba(255, 255, 255, 1)',
+                'rgba(255, 255, 255, 1)',
+                'rgba(255, 255, 255, 1)',
+                'rgba(255, 255, 255, 1)',
+                'rgba(255, 255, 255, 1)',
+                'rgba(255, 255, 255, 1)',
+                'rgba(255, 255, 255, 1)',
+                'rgba(255, 255, 255, 1)',
+                'rgba(255, 255, 255, 1)'
+            ],
+            borderWidth: 2
+        }]
+    },
+    options: {
+        maintainAspectRatio: false,
+        scales: {
+            xAxes: [{
+                ticks: {
+                    // stepValue: 1,
+                    // suggestedMin: 0,
+                    beginAtZero: true,
+                    callback: function(value) {if (value % 1 === 0) {return value;}}
+                }
+            }]
+        }
+    }
+});
